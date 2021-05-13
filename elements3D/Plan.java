6d023acd2094fbb99e53dcdd9f2a4fe8ee8d4267
Edgar Remy <83783043+edgaremy@users.noreply.github.com>
2021-05-13 12:28:35 +0200

    Update Plan.java

--- elements3D/Plan.java
+++ elements3D/Plan.java
@@ -16,7 +16,6 @@ import rayTracing.Lumiere;
 
 public class Plan implements Objet3D, Serializable {
 	
-	
 	private static final long serialVersionUID = 7303995654083808341L;
 	private static int compteur = 0; // compteur pour les noms par défaut
 
